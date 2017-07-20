8d01f54f70756b85fa2529fbc861ff0b5d93fd5c
Aleksey Shagraev <alex-shagraev@ya.ru>
2017-07-20 07:20:01 +0300

    much better interfaces
    - rewrite using "calculator" classes
    - print errors on different steps

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <numeric>
 #include <vector>
+#include <memory>
+#include <sstream>
 
 class TKahanAccumulator {
 private:
@@ -32,54 +34,136 @@ public:
     }
 };
 
-double DummyCovariation(const std::vector<double>& x, const std::vector<double>& y) {
-    const double sumCrossProducts = std::inner_product(x.begin(), x.end(), y.begin(), 0.);
-    const double sumX = std::accumulate(x.begin(), x.end(), 0.);
-    const double sumY = std::accumulate(y.begin(), y.end(), 0.);
+class ICovariationCalculator {
+private:
+public:
+    virtual void Add(const double x, const double y) = 0;
+    virtual double Covariation() const = 0;
+    virtual std::string Name() const = 0;
+};
 
-    return (sumCrossProducts - sumX * sumY / x.size()) / x.size();
-}
-
-double KahanCovariation(const std::vector<double>& x, const std::vector<double>& y) {
-    TKahanAccumulator sumCrossProducts;
-    TKahanAccumulator sumX;
-    TKahanAccumulator sumY;
-
-    for (size_t i = 0; i < x.size(); ++i) {
-        sumCrossProducts += x[i] * y[i];
-        sumX += x[i];
-        sumY += y[i];
+template <class TAccumulatorType>
+class TTypedCovariationCalculator : public ICovariationCalculator {
+private:
+    size_t Count = 0;
+    TAccumulatorType SumX = 0.;
+    TAccumulatorType SumY = 0.;
+    TAccumulatorType SumProducts = 0.;
+public:
+    void Add(const double x, const double y) override {
+        ++Count;
+        SumX += x;
+        SumY += y;
+        SumProducts += x * y;
     }
 
-    return (sumCrossProducts - sumX * sumY / x.size()) / x.size();
-}
-
-double SmartCovariation(const std::vector<double>& x, const std::vector<double>& y) {
-    double sumProducts = 0.;
-    double xMean = 0.;
-    double yMean = 0.;
-
-    for (size_t i = 0; i < x.size(); ++i) {
-        xMean += (x[i] - xMean) / (i + 1);
-        sumProducts += (x[i] - xMean) * (y[i] - yMean);
-        yMean += (y[i] - yMean) / (i + 1);
+    double Covariation() const override {
+        return ((double) SumProducts - (double) SumX * (double) SumY / Count) / Count;
     }
 
-    return sumProducts / x.size();
-}
+    std::string Name() const override;
+};
+
+using TDummyCovariationCalculator = TTypedCovariationCalculator<double>;
+using TKahanCovariationCalculator = TTypedCovariationCalculator<TKahanAccumulator>;
+
+std::string TDummyCovariationCalculator::Name() const {
+    return "Dummy";
+};
+
+std::string TKahanCovariationCalculator::Name() const {
+    return "Kahan";
+};
+
+class TWelfordCovariationCalculator : public ICovariationCalculator {
+private:
+    size_t Count = 0;
+    double MeanX = 0.;
+    double MeanY = 0.;
+    double SumProducts = 0.;
+public:
+    void Add(const double x, const double y) override {
+        ++Count;
+        MeanX += (x - MeanX) / Count;
+        SumProducts += (x - MeanX) * (y - MeanY);
+        MeanY += (y - MeanY) / Count;
+    }
+
+    double Covariation() const override {
+        return SumProducts / Count;
+    }
+
+    std::string Name() const override {
+        return "Welford";
+    }
+};
 
 double Error(const double target, const double value) {
     return fabs(value - target) / fabs(target);
 }
 
+class TPrinter {
+private:
+    std::string Title;
+
+    std::vector<std::string> Columns;
+    std::vector<std::vector<std::string>> Rows;
+public:
+    TPrinter(const std::string& title)
+        : Title(title)
+    {
+    }
+
+    void AddColumn(const std::string& name) {
+        Columns.push_back(name);
+    }
+
+    void AddRow() {
+        Rows.push_back(std::vector<std::string>());
+    }
+
+    template <class TValue>
+    void AddToRow(const TValue& value) {
+        std::stringstream ss;
+        ss.precision(10);
+        ss << value;
+        Rows.back().push_back(ss.str());
+    }
+
+    void Print() const {
+        auto printRow = [&](const std::vector<std::string>& row){
+            const size_t columnWidth = 25;
+
+            std::stringstream rowStream;
+            for (const std::string& element : row) {
+                PrintNextColumn(rowStream, element, columnWidth);
+            }
+
+            printf("%s\n", rowStream.str().c_str());
+        };
+
+        printf("%s\n", Title.c_str());
+        printRow(Columns);
+        for (const std::vector<std::string>& row : Rows) {
+            printRow(row);
+        }
+    }
+private:
+    template <class TValue>
+    void PrintNextColumn(std::stringstream& out, const TValue& value, size_t columnWidth) const {
+        const size_t startLength = out.str().size();
+        out << value;
+        const size_t currentLength = out.str().size();
+        for (size_t i = currentLength; i < startLength + columnWidth; ++i) {
+            out << " ";
+        }
+    }
+};
+
 int main() {
-    size_t interestingSizes[] = { 10000, 100000, 1000000, 5000000 };
     double interestingMeans[] = { 100, 100000, 10000000 };
 
     for (const double mean : interestingMeans) {
-        std::vector<double> x;
-        std::vector<double> y;
-
         const double xMean = mean;
         const double yMean = mean;
 
@@ -88,36 +172,41 @@ int main() {
 
         const double actualCovariation = xDiff * yDiff;
 
-        printf("mean: %10lf\n", mean);
-        for (size_t interestingSize : interestingSizes) {
-            x.reserve(interestingSize);
-            y.reserve(interestingSize);
+        std::vector<std::shared_ptr<ICovariationCalculator>> calculators;
+        calculators.push_back(std::shared_ptr<TDummyCovariationCalculator>(new TDummyCovariationCalculator()));
+        calculators.push_back(std::shared_ptr<TKahanCovariationCalculator>(new TKahanCovariationCalculator()));
+        calculators.push_back(std::shared_ptr<TWelfordCovariationCalculator>(new TWelfordCovariationCalculator()));
 
-            for (size_t i = x.size(); i < interestingSize; ++i) {
-                x.push_back(xMean + xDiff);
-                y.push_back(yMean + yDiff);
+        TPrinter printer("mean: " + std::to_string(mean));
+        printer.AddColumn("Count");
+        for (auto&& calculator : calculators) {
+            printer.AddColumn(calculator->Name());
+        }
 
-                if (rand() % 2) {
-                    xDiff = -xDiff;
-                    yDiff = -yDiff;
+        const size_t count = 10000000;
+        for (size_t i = 0; i < count; ++i) {
+            if (i && (i % (count / 100) == 0)) {
+                printer.AddRow();
+                printer.AddToRow(i);
+                for (auto&& calculator : calculators) {
+                    const double calculatedCovariation = calculator->Covariation();
+                    const double error = Error(actualCovariation, calculatedCovariation) * 100;
+                    printer.AddToRow(error);
                 }
             }
 
-            const double dummyCovariation = DummyCovariation(x, y);
-            const double kahanCovariation = KahanCovariation(x, y);
-            const double smartCovariation = SmartCovariation(x, y);
+            if (rand() % 2) {
+                xDiff = -xDiff;
+                yDiff = -yDiff;
+            }
 
-            const double dummyError = Error(actualCovariation, dummyCovariation) * 100;
-            const double kahanError = Error(actualCovariation, kahanCovariation) * 100;
-            const double smartError = Error(actualCovariation, smartCovariation) * 100;
-
-            printf("\tsize: %lu\n", interestingSize);
-            printf("\t\tactual covariation: %.10lf\n", actualCovariation);
-            printf("\t\tsmart covariation:  %.10lf (error: %.10lf)\n", smartCovariation, smartError);
-            printf("\t\tkahan covariation:  %.10lf (error: %.10lf)\n", kahanCovariation, kahanError);
-            printf("\t\tdummy covariation:  %.10lf (error: %.10lf)\n", dummyCovariation, dummyError);
-            printf("\n");
+            for (auto&& calculator : calculators) {
+                calculator->Add(xMean + xDiff, yMean + yDiff);
+            }
         }
+
+        printer.Print();
+        printf("\n\n");
     }
 
     return 0;
